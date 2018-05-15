d34e63b71c2858663a109630ee04d505fad44bcf
hongs <hongs@DESKTOP-6C2JF36>
2018-05-15 21:39:00 +0900

    first commit !

---
+++ 1st/aa.py
@@ -0,0 +1,16 @@
+# def hi(name):
+#     print('Hi ' + name + '!')
+# 
+# girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+# for name in girls:
+#     hi(name)
+#     print('Next girl')
+
+def hi(name):
+    print(name)
+    
+girls = ['a', 'b', 'c', 'd']
+
+for name in girls:
+    hi(name)
+    print('next')
